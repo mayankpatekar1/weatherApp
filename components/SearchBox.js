50ee22bc44310501dc98cfb4e611d4dd83f8416c
mayankpatekar1 <mayankpatekar11234@gmail.com>
2022-04-02 19:17:00 +0530

    final hosting

--- components/SearchBox.js
+++ components/SearchBox.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import cities from "../lib/city.list.json";
-import Link from "next/Link";
+import Link from "next/link";
 import Router from 'next/router';
 
 export default function SearchBox({placeholder}){
